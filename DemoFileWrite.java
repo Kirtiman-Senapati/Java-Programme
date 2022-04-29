c1253234926b39e23b7248bfb628247d90f18d1e
Kirtiman-Senapati <kirtimansenapati29@gmail.com>
2022-04-29 22:46:24 +0530

    writing file

---
+++ DemoFileWrite.java
@@ -0,0 +1,20 @@
+ import java.io.FileWriter;
+ import java.io.IOException;
+ class DemoFileWrite
+ {
+    public static void main(String[] args) 
+    {
+        try
+        {
+           FileWriter v= new FileWriter("C:\\Program Files (x86)\\createFile.txt");
+            v.write("\n My name is kirtiman \n my aim is to do something for country ");
+            v.close();
+            System.out.println("comtent is successfully wrote in File");
+        }
+        catch(IOException e)
+        {
+         System.out.println(e);
+         e.printStackTrace();
+        }
+    }   
+}
