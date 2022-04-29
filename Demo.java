5fb5b1db2dd5dbaed242441f0ecd5c3d22f14149
Kirtiman-Senapati <kirtimansenapati29@gmail.com>
2022-04-29 22:43:41 +0530

    overriding method

---
+++ Demo.java
@@ -0,0 +1,24 @@
+ class D
+ {
+    void drink ()
+    {
+      System.out.println("i drink cofee"); 
+    }
+}
+class M extends D
+{
+    void drink()
+    {
+      // super.drink();
+      System.out.println("i drink tea");
+    }
+
+}
+class Demo
+{
+ public static void main(String[] args) 
+{  
+  D R = new D();
+  R.drink();
+}
+}
