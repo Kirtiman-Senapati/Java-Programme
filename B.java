58c68abb21f5124d739f6c31d07aa6c409737421
Kirtiman-Senapati <kirtimansenapati29@gmail.com>
2022-05-01 17:33:03 +0530

    use override different manner

---
+++ B.java
@@ -0,0 +1,27 @@
+class cat 
+{
+    void eat() 
+    {
+        int speedlimit = 90;
+    }
+
+}
+
+class Dog extends cat 
+{
+
+   // public char[] speedlimit;
+    void eat() 
+    {
+        int speedlimit = 150;
+    }
+}
+
+class B 
+{
+    public static void main(String[] args) 
+    {
+        Dog obj = new Dog();
+        System.out.println(obj.equals(obj));
+    }
+}
